0b81392f5eb45ef2ea924cf5bf974923e654448d
Anuj goyal <59957810+royalanuj03@users.noreply.github.com>
2023-02-01 20:21:21 +0530

    Code1

---
+++ r1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class r1{
+
+    public static void pd(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        pd(n-1);
+    }
+    public static void main(String[] args){
+        pd(5);
+    }
+}
