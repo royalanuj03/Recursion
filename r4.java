61c5df0934c419234600607f5703a46ece75f634
Anuj goyal <59957810+royalanuj03@users.noreply.github.com>
2023-02-02 19:34:27 +0530

    Code4

---
+++ r4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class r4{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int ans = fact(n);
+        System.out.println(ans);
+    }
+
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fnm1;
+        return fn;
+    }
+}
